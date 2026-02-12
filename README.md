2cbe782be820ef861417321ad9a5fa100aacbebb
turip0 <10fipik@gmail.com>
2026-02-12 16:02:14 +0200

    Initial comment

---
+++ README.md
@@ -0,0 +1,28 @@
+#Dotfiles
+
+##This are my dotfiles from my dell laptop running arch
+
+To install run 
+
+```
+```          
+``` 
+$git clone https://github.com/turip0/dotfiles.git
+$cd dotfiles
+$./install.sh
+```
+
+Dotfiles include:
+```
+-> zshrc 
+-> custom scripts
+-> gtk themes 
+-> configs
+```
+
+```
+```
+```
+```
+```
+```
--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#pacman -Qqe | grep stow > cat >> ~/tmp
+CYAN="\e[0;36;1m"
+RESET="\e[0m"
+
 mapfile -t stoww < <(pacman -Qqe | grep stow)
 case ${stoww[0]} in
 'stow') ;;
 *)
-  printf "install stow? n/y (deafult) "
+  printf "${CYAN}install stow? n/y (deafult)${RESET} "
   read -r install
   case $install in
   'y')
@@ -18,4 +20,34 @@ case ${stoww[0]} in
   esac
   ;;
 esac
-stow .
+
+mapfile -t gitt < <(pacman -Qqe | grep git)
+case ${gitt[1]} in
+'git') ;;
+*)
+  printf "${CYAN}install git? n/y (deafult)${RESET} "
+  read -r install
+  case $install in
+  'y')
+    sudo pacman -S git
+    ;;
+  'n') ;;
+  'no') ;;
+  *)
+    sudo pacman -S git
+    ;;
+  esac
+  ;;
+esac
+
+printf "${CYAN}Do you want to replace old configs? Y/n (deafult)${RESET} "
+read -r safe
+case $safe in
+'y')
+  stow --adopt .
+  ;;
+*)
+  stow .
+  ;;
+esac
+printf "\n \e[0;32;1m dotfiles symlinked successfully!! \e[0m"
